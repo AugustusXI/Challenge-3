f27891fd1608060db23fcbcac97aa3320f4f9bd5
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-05-08 23:59:35 -0600

    Second Commit
    Second commit

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var specialChar = "~`!@#$%^&*()_+-={}[]|'.,<>/?";
 var numbers = "0123456789";
+var passwordLength;
 
 // prompt box on button click to define password criteria
-function promptMe(){
-var length = prompt("Choose a password length between 8 and 128 characters"){
+function passwordLength(){
+let passwordLength = prompt("Choose a password length between 8 and 128 characters");{
   if (length<8) {
-    alert("Password must be at least 8 characters")
-    promptMe();
-  }
+    alert("Password must be at least 8 characters");
+    passwordLength();
+  } else if (passwordLength>128) {
+    alert("Password must be no more than 128 characters");
+    passwordLength();
+  } else if (isNaN(passwordLength))
+    alert("Password length must be a number between 8 and 128");
+    passwordLength();
+  } 
+  return passwordLength;
 }
 
-var askupperCase = confirm("Would you like to include uppercase letters?")
-var asklowerCase = confirm("Would you like to include lowercase letters?")
-var askspecialChar = confirm("Would you like to include special characters?")
-var asknumbers = confirm("Would you like to include numbers?")
-
+var askupperCase = confirm("Would you like to include uppercase letters?\n Select 'ok' for yes, or 'cancel' for no")
+var asklowerCase = confirm("Would you like to include lowercase letters?\n Select 'ok' for yes, or 'cancel' for no")
+var askspecialChar = confirm("Would you like to include special characters?\n Select 'ok' for yes, or 'cancel' for no")
+var asknumbers = confirm("Would you like to include numbers?\n Select 'ok' for yes, or 'cancel' for no")
 }
 // Write password to the #password input
 function writePassword() {
